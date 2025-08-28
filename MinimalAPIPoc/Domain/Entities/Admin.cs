dc0a0c92e6f1c0fa078f1a0055de43d2814882f9
Fernando Serpa <serpaef@outlook.com>
2025-08-27 21:19:33 -0300

    Create Admin Entity to start building application db context

---
+++ MinimalAPIPoc/Domain/Entities/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPIPoc.Domain.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Role { get; set; } = default!;
+    }
+}
