9116baca61b7fdae41a7688e388e780c7093f3fa
Fernando Serpa <serpaef@outlook.com>
2025-08-27 21:47:43 -0300

    Dependency injection of db configuration.

---
+++ MinimalAPIPoc/Infrastructure/Db/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPoc.Domain.Entities;
+
+namespace MinimalAPIPoc.Infrastructure.Db
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Admin> Admins { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if( optionsBuilder.IsConfigured ) return;
+
+            var connectionString = _configuration.GetConnectionString("mysql")?.ToString();
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString)
+                );
+            }
+        }
+    }
+}
--- MinimalAPIPoc/Program.cs
+++ MinimalAPIPoc/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalAPIPoc.Domain.DTO;
+using MinimalAPIPoc.Infrastructure.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Entrypoint");
