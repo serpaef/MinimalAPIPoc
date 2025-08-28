1a0b6f758d27785d9ed5c0b9ad1134d97ea96199
Fernando Serpa <serpaef@outlook.com>
2025-08-27 21:55:33 -0300

    Add Admin entity and migration for database schema
    Introduce a new `Admin` entity with properties for `Id`, `Username`, `Password`, and `Role`, including data annotations for validation. Create migration files to establish the `Admins` table in the database. Update the model snapshot to reflect the new entity configuration in the Entity Framework Core context.

--- MinimalAPIPoc/Domain/Entities/Admin.cs
+++ MinimalAPIPoc/Domain/Entities/Admin.cs
@@ -1,10 +1,23 @@
-﻿namespace MinimalAPIPoc.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinimalAPIPoc.Domain.Entities
 {
     public class Admin
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
         public string Username { get; set; } = default!;
+
+        [Required]
+        [StringLength(25)]
         public string Password { get; set; } = default!;
+
+        [StringLength(10)]
         public string Role { get; set; } = default!;
     }
 }
---
+++ MinimalAPIPoc/Migrations/20250828005410_AdminMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalAPIPoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "varchar(25)", maxLength: 25, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Role = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
