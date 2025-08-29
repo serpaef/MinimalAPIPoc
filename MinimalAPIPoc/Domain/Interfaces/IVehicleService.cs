d9a4c3547c0f863f4071044c715ce5a85b92ea33
Fernando Serpa <serpaef@outlook.com>
2025-08-28 22:16:26 -0300

    Add IVehicleService interface and VehicleService implementation
    Introduces the `IVehicleService` interface for vehicle management, including methods for retrieving, creating, updating, and deleting vehicles. Implements the `VehicleService` class that provides concrete functionality using `ApplicationDbContext`, with added support for filtering by name and make, as well as pagination for vehicle retrieval.

---
+++ MinimalAPIPoc/Domain/Interfaces/IVehicleService.cs
@@ -0,0 +1,13 @@
+﻿using MinimalAPIPoc.Domain.Entities;
+
+namespace MinimalAPIPoc.Domain.Interfaces
+{
+    public interface IVehicleService
+    {
+        List<Vehicle> GetAllVehicles(int page = 1, string? name = null, string? make = null);
+        Vehicle? GetVehicleById(int id);
+        void CreateVehicle(Vehicle vehicle);
+        void UpdateVehicle(Vehicle vehicle);
+        void DeleteVehicle(int id);
+    }
+}
---
+++ MinimalAPIPoc/Domain/Services/VehicleService.cs
@@ -0,0 +1,62 @@
+﻿using MinimalAPIPoc.Domain.Entities;
+using MinimalAPIPoc.Domain.Interfaces;
+using MinimalAPIPoc.Infrastructure.Db;
+
+namespace MinimalAPIPoc.Domain.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateVehicle(Vehicle vehicle)
+        {
+            _context.Vehicles.Add(vehicle);
+            _context.SaveChanges();
+        }
+
+        public void DeleteVehicle(int id)
+        {
+            _context.Vehicles.Remove(new Vehicle { Id = id });
+            _context.SaveChanges();
+        }
+
+        public List<Vehicle> GetAllVehicles(int page = 1, string? name = null, string? make = null)
+        {
+            var vehicleQuery = _context.Vehicles.AsQueryable();
+            
+            if(!string.IsNullOrEmpty(name))
+            {
+                vehicleQuery = vehicleQuery.Where(v => v.Name.ToLower().Contains(name.ToLower()));
+            }
+
+            if(!string.IsNullOrEmpty(make))
+            {
+                vehicleQuery = vehicleQuery.Where(v => v.Make.ToLower().Contains(make.ToLower()));
+            }
+
+            const int PAGE_SIZE = 10;
+
+            vehicleQuery = vehicleQuery
+                .Skip((page - 1) * PAGE_SIZE)
+                .Take(PAGE_SIZE);
+
+            return vehicleQuery.ToList();
+        }
+
+        public Vehicle? GetVehicleById(int id)
+        {
+            return _context.Vehicles.Find(id);
+        }
+
+        public void UpdateVehicle(Vehicle vehicle)
+        {
+            _context.Vehicles.Update(vehicle);
+            _context.SaveChanges();
+        }
+    }
+}
