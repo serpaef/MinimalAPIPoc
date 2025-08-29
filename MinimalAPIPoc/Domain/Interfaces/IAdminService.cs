42da5de483be411c883e05cd776da272d20d67f4
Fernando Serpa <serpaef@outlook.com>
2025-08-28 21:39:58 -0300

    Add IAdminService interface and AdminService implementation
    Introduces the `IAdminService` interface with a `Login` method
    that accepts a `LoginDTO` and returns an `Admin` object.
    Implements the `AdminService` class, which uses
    Entity Framework to authenticate an admin by username and
    password, ensuring retrieval of the admin object.

---
+++ MinimalAPIPoc/Domain/Interfaces/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using MinimalAPIPoc.Domain.DTO;
+using MinimalAPIPoc.Domain.Entities;
+
+namespace MinimalAPIPoc.Domain.Interfaces
+{
+    public interface IAdminService
+    {
+        Admin Login(LoginDTO loginDTO);
+
+    }
+}
---
+++ MinimalAPIPoc/Domain/Services/AdminService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPoc.Domain.DTO;
+using MinimalAPIPoc.Domain.Entities;
+using MinimalAPIPoc.Domain.Interfaces;
+using MinimalAPIPoc.Infrastructure.Db;
+
+namespace MinimalAPIPoc.Domain.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Admin Login(LoginDTO loginDTO)
+        {
+            var admin = _db.Admins.Where(a => a.Username == loginDTO.Username && a.Password == loginDTO.Password).FirstOrDefault();
+
+            return admin!;
+        }
+    }
+}
